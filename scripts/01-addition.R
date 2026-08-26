aaf0a5d4bfeecf4fc5faa3ba9d5dbb696d45408d
kgill660 <kgill660@mtroyal.ca>
2026-08-26 14:32:47 -0600

    Create 01-addition.R

---
+++ scripts/01-addition.R
@@ -0,0 +1,32 @@
+
+# Write some code
+
+# What is 2 + 4?
+answer <- 2 + 4
+
+# What is 9 + 9
+answer2 <- 17
+
+# What is 5 x 5
+answer3 <- 25
+
+#builderbernardwashere
+answer4 <- 1
+
+<<<<<<< HEAD
+#so was I
+answer5 <- 6
+=======
+#bigdawgwasalsohere
+<<<<<<< HEAD
+answer5 <- 21
+
+#kgill660
+answer6 <-22
+=======
+answer5 <- 67
+
+#What's 9 + 10
+answer6 <- 21
+>>>>>>> 45433fc3fa82b588dd41b7e47a6cbc4f8ce5a5c2
+>>>>>>> 039582ec9a2f1cddec17c7f706bc7f55145f46b9
